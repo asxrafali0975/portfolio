87134324e452ca7742fe655f9795a821fc523e78
asxrafali0975 <2023bca113@axiscolleges.in>
2024-07-11 17:55:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+const options = {
+    strings: ["full-stack developer ^1000", "Web-Designer ^1000", "Video Editor ^1000"],
+    typeSpeed: 40,
+    loop: true,
+    loopCount: Infinity,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 100,
+    showCursor: false,
+};
+
+var typed = new Typed("#typed-output", options);
